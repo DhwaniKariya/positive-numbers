dea45917b82118aa0c77d77980a46292b37e23d6
DhwaniKariya <67733431+DhwaniKariya@users.noreply.github.com>
2020-07-04 15:30:19 +0530

    positive_num.py

---
+++ positive_num.py
@@ -0,0 +1,14 @@
+list1=[12,-13,14,-15,16,-17]
+list2=[-95,94,-93,92,-91,90]
+print(list1)
+print(list2)
+
+print("positive numbers in list1 are")
+for number in list1:
+     if number>1:
+        print(number)
+
+print("positive numbers in list2 are")
+for num in list2:
+    if num>1:
+        print(num)
